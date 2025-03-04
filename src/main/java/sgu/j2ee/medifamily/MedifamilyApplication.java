838b49a468ac2b606825fbb9ffc92cc5d421ec53
Huu Hoang <huuhoag1412@gmail.com>
2025-03-04 22:45:12 +0700

    Initialize Medifamily project with Spring Boot setup and necessary configurations

---
+++ src/main/java/sgu/j2ee/medifamily/MedifamilyApplication.java
@@ -0,0 +1,13 @@
+package sgu.j2ee.medifamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedifamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedifamilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sgu/j2ee/medifamily/MedifamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package sgu.j2ee.medifamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
