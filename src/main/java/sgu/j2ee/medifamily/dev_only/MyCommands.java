16a93cd05b63d16bdf31d0eeb3868503e18db545
Huu Hoang <huuhoag1412@gmail.com>
2025-04-11 00:28:33 +0700

    fix not gen api

--- src/main/java/sgu/j2ee/medifamily/dev_only/MyCommands.java
+++ src/main/java/sgu/j2ee/medifamily/dev_only/MyCommands.java
@@ -1,10 +1,16 @@
 package sgu.j2ee.medifamily.dev_only;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -43,5 +49,31 @@ public class MyCommands {
 			user = userRepository.findAll(Pageable.ofSize(1)).getContent().get(0);
 		}
 		return jwtService.generateToken(user);
+	}
+
+	@ShellMethod("Gen OpenApi")
+	public void genOpenApi() {
+		try {
+			if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+				log.info("Skipping openapi.json generation in production mode.");
+				return;
+			}
+
+			log.info("Generating openapi.json file...");
+			Thread.sleep(2000);
+
+			RestTemplate restTemplate = new RestTemplate();
+			String apiUrl = "http://localhost:8080" + "/v3/api-docs";
+			String openApiContent = restTemplate.getForObject(apiUrl, String.class);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+			String prettyJson = objectMapper.writeValueAsString(objectMapper.readTree(openApiContent));
+
+			Files.write(Paths.get("openapi.json"), prettyJson.getBytes());
+			log.info("Successfully generated prettified openapi.json file.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
