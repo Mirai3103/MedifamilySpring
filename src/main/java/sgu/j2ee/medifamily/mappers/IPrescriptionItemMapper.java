44896f47c9f6803f092dd06e3d484addfb42b8c1
Huu Hoang <huuhoag1412@gmail.com>
2025-04-11 00:17:33 +0700

    fix something

---
+++ src/main/java/sgu/j2ee/medifamily/mappers/IPrescriptionItemMapper.java
@@ -0,0 +1,21 @@
+package sgu.j2ee.medifamily.mappers;
+
+import java.util.List;
+
+import org.mapstruct.*;
+
+import sgu.j2ee.medifamily.dtos.PrescriptionItemDto;
+import sgu.j2ee.medifamily.entities.PrescriptionItem;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface IPrescriptionItemMapper {
+	PrescriptionItem toEntity(PrescriptionItemDto prescriptionItemDto2);
+
+	PrescriptionItemDto toDto(PrescriptionItem prescriptionItem);
+
+	List<PrescriptionItemDto> toDto(List<PrescriptionItem> prescriptionItem);
+
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	PrescriptionItem partialUpdate(PrescriptionItemDto prescriptionItemDto2,
+			@MappingTarget PrescriptionItem prescriptionItem);
+}
